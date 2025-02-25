8b61479c444f3dd2b69f5f11c6e6a1f6db9b58f8
sidney-pauly <sidney@friedrich-pauly.de>
2025-02-25 20:03:05 +0000

    added i2c test

--- test_i2c.py
+++ test_i2c.py
@@ -1,2 +1,11 @@
+from smbus import SMBus
 
+BNO_ADDR = 0x40
+BNO_OPR_MODE_ADDR = 0x3D
+    
+i2cbus = SMBus(1)
+
+operating_mode = i2cbus.read_byte_data(BNO_ADDR, BNO_OPR_MODE_ADDR)
+
+print(operating_mode)
 
