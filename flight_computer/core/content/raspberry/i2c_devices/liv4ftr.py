26646881e355d1feafd7889171bbeb3b8b5b20f9
sidney-pauly <sidney@friedrich-pauly.de>
2025-08-22 03:45:46 +0100

    Adding first gps part prototype

---
+++ flight_computer/core/content/raspberry/i2c_devices/liv4ftr.py
@@ -0,0 +1,72 @@
+from asyncio import Task
+import asyncio
+import struct
+from smbus3 import SMBus
+
+
+from datetime import timedelta
+from logging import _nameToLevel
+from typing import Callable, Iterable, Tuple, Type, Union
+from uuid import UUID
+from flight_computer.core.content.raspberry.i2c import RaspberryI2CInterface
+from flight_computer.core.logic.rocket_definition import Part, Rocket
+
+
+class LIV4FTR(Part):
+
+    type = 'Sensor.GPS'
+
+    i2c_device_id = 0x3A
+
+    min_update_period = timedelta(milliseconds=10)
+    min_measurement_period = timedelta(milliseconds=5)
+
+    def __init__(self, _id: UUID, name: str, parent: Union[Part, Rocket, None], i2c: RaspberryI2CInterface):
+
+        super().__init__(_id, name, parent, list()) # type: ignore
+
+        i2c.i2c_loop_callbacks.add(self.make_i2c_callback())
+
+        # self.M_TEMP = self.measurement_index_lookup['temp']
+
+    def make_measurement_shape(self):
+        return [
+            *super().make_measurement_shape(),           
+        ]
+
+    def make_accepted_commands(self):
+        return [
+            *super().make_accepted_commands()
+        ]
+    
+    def make_command_callbacks(self):
+        return [
+            *super().make_command_callbacks()
+        ]
+   
+    def update(self, now, iteration):
+
+        return
+    
+    def make_i2c_callback(self):
+
+        def i2c_callback(i2c, now, iteration):
+
+            buffer = list()
+
+            while True:
+                # Read NMEA messages from gps
+                char = i2c.read_byte_data(self.i2c_device_id, 0xFF)
+                if char > 127:
+                    continue
+                # No more messages
+                if char == 0:
+                    break
+                buffer.append(char)
+            
+            messages = bytes(buffer).decode('ascii').splitlines()
+
+            for msg in messages:
+                print(msg)
+        
+        return i2c_callback
